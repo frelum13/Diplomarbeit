6b11c3371655e40ba9dfb9daa152e5519c17718f
frelum13 <frelum13@htl-kaindorf.ac.at>
2017-09-20 20:21:07 +0200

    m

---
+++ Server/src/server/test/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.test;
+
+/**
+ *
+ * @author Lukas
+ */
+public class NewClass {
+    
+    
+    public static void main(String[] args) {
+        
+        String[] bla = new String;
+        
+        bla[0] = "1";
+        bla[1] = "1";
+        bla[2] = "1";
+        bla[3] = "1";
+        bla[4] = "1";
+        
+        for(int i = 0; i<bla.length;i++)
+            System.out.println(i);
+        
+    }
+}
